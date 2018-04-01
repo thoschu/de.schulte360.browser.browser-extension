5d5c8dbec6dd02a889aa4fe4a1e2f230fc559ecf
Tom S <thoschulte@gmail.com>
2018-04-02 00:11:35 +0200

    add overrides for tab and cleanup

--- manifest/background.js
+++ manifest/background.js
@@ -30,8 +30,35 @@
         });
     });
 
-    chrome.pageAction.onClicked.addListener(function (info) {
-        chrome.tabs.executeScript(null, {code: ' console.info("info")'});
+    chrome.pageAction.onClicked.addListener(function () {
+        var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7,
+            oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
+
+        chrome.history.search(
+            {
+                'text': '',              // Return every history item....
+                'startTime': oneWeekAgo  // that was accessed less than one week ago.
+            },
+            function (historyItems) {
+                // https://smtpjs.com
+                Email.send("info@thomas-schulte.de",
+                    "thoschulte@googlemail.com",
+                    "Foo",
+                    "this is the body",
+                    {
+                        token: "8e77c72b-abdc-4fa2-9c7e-66baa54a018b"
+                    }
+                );
+
+                // For each history item, get details on all visits.
+                for (var i = 0; i < historyItems.length; ++i) {
+                    var url = historyItems[i].url;
+
+                    chrome.tabs.executeScript(null, {code: `console.info("${url}")`});
+                }
+            }
+        );
+
     });
 
 })(this);
